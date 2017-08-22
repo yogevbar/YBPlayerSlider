1418b7c0ff09860b033b3f35ce194a6fd8ffb99e
yogevbar <yogevbarber@gmail.com>
2017-08-22 13:54:30 +0300

    working project

---
+++ YBPlayerSlider/YBPlayerSlider.h
@@ -0,0 +1,19 @@
+//
+//  YBPlayerSlider.h
+//  YBPlayerSlider
+//
+//  Created by Yogev Barber on 22/08/2017.
+//  Copyright © 2017 yogev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YBPlayerSlider.
+FOUNDATION_EXPORT double YBPlayerSliderVersionNumber;
+
+//! Project version string for YBPlayerSlider.
+FOUNDATION_EXPORT const unsigned char YBPlayerSliderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YBPlayerSlider/PublicHeader.h>
+#import <YBPlayerSlider/YBSlider.h>
+
